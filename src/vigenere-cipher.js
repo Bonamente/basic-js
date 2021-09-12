1d22602c5d96a3660aba72f9e75618d0d6ba8aa4
Bonamente <56506035@protonmail.com>
2021-09-12 19:47:27 +0500

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,90 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+const tabulaRecta = { 
+  A: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  B: 'BCDEFGHIJKLMNOPQRSTUVWXYZA',
+  C: 'CDEFGHIJKLMNOPQRSTUVWXYZAB',
+  D: 'DEFGHIJKLMNOPQRSTUVWXYZABC',
+  E: 'EFGHIJKLMNOPQRSTUVWXYZABCD',
+  F: 'FGHIJKLMNOPQRSTUVWXYZABCDE',
+  G: 'GHIJKLMNOPQRSTUVWXYZABCDEF',
+  H: 'HIJKLMNOPQRSTUVWXYZABCDEFG',
+  I: 'IJKLMNOPQRSTUVWXYZABCDEFGH',
+  J: 'JKLMNOPQRSTUVWXYZABCDEFGHI',
+  K: 'KLMNOPQRSTUVWXYZABCDEFGHIJ',
+  L: 'LMNOPQRSTUVWXYZABCDEFGHIJK',
+  M: 'MNOPQRSTUVWXYZABCDEFGHIJKL',
+  N: 'NOPQRSTUVWXYZABCDEFGHIJKLM',
+  O: 'OPQRSTUVWXYZABCDEFGHIJKLMN',
+  P: 'PQRSTUVWXYZABCDEFGHIJKLMNO',
+  Q: 'QRSTUVWXYZABCDEFGHIJKLMNOP',
+  R: 'RSTUVWXYZABCDEFGHIJKLMNOPQ',
+  S: 'STUVWXYZABCDEFGHIJKLMNOPQR',
+  T: 'TUVWXYZABCDEFGHIJKLMNOPQRS',
+  U: 'UVWXYZABCDEFGHIJKLMNOPQRST',
+  V: 'VWXYZABCDEFGHIJKLMNOPQRSTU',
+  W: 'WXYZABCDEFGHIJKLMNOPQRSTUV',
+  X: 'XYZABCDEFGHIJKLMNOPQRSTUVW',
+  Y: 'YZABCDEFGHIJKLMNOPQRSTUVWX',
+  Z: 'ZABCDEFGHIJKLMNOPQRSTUVWXY'
+};
+
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.type = isDirect ? 'direct' : 'reverse';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    const messageUC = message.toUpperCase();
+    const keyUC = key.match(/[A-Z]/gi).join('').toUpperCase();
+   
+    let encryptedMessage = '';
+    let specialCharacterCount = 0;
+
+    for (let i = 0; i < messageUC.length; i += 1){
+      let keyLetter = (i - specialCharacterCount) % keyUC.length;
+      let keyIndex = tabulaRecta.A.indexOf(keyUC[keyLetter]);
+
+      if (tabulaRecta[messageUC[i]]) {
+        encryptedMessage += tabulaRecta[messageUC[i]][keyIndex];
+      } else {
+        encryptedMessage += messageUC[i];
+        specialCharacterCount += 1;
+      }
+    }
+
+    return this.type === 'direct' 
+      ? encryptedMessage 
+      : encryptedMessage.split('').reverse().join('');
   }
-}
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    const encryptedMessageUC = encryptedMessage.toUpperCase();
+    const keyUC = key.match(/[A-Z]/gi).join('').toUpperCase();
+
+    let decryptedMessage = '';
+    let specialCharacterCount = 0;
+
+    for (let i = 0; i < encryptedMessageUC.length; i += 1) {
+      let keyLetter = (i - specialCharacterCount) % keyUC.length;
+      let keyRow = tabulaRecta[keyUC[keyLetter]];
+
+      if (keyRow.indexOf(encryptedMessageUC[i]) !== -1) {
+        decryptedMessage += tabulaRecta.A[keyRow.indexOf(encryptedMessageUC[i])];
+      } else {
+        decryptedMessage += encryptedMessageUC[i];
+        specialCharacterCount += 1;
+      }
+    }
+
+    return this.type === 'direct' 
+      ? decryptedMessage 
+      : decryptedMessage.split('').reverse().join('');
+  }
+};
