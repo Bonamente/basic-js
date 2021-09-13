f0048af45ffb53c7900d184cab4274342c64d820
Bonamente <56506035@protonmail.com>
2021-09-13 11:24:04 +0500

    feat: add st-encode-line solution

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  const map = new Map();
+  let result = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    const prevChar = str[i - 1];
+    const nextChar = str[i + 1];
+
+    if (map.has(char) && (char === prevChar || char === nextChar)) {
+      map.set(char, map.get(char) + 1);
+    } else if (!map.has(char) && char === nextChar) {
+      map.set(char, 1)
+    } else {
+      map.set([i], 1);
+    }
+  }
+
+  for (let [value, count] of map) {
+    if (count === 1) {
+      result += str[value];
+    } else {
+      result += `${count}${value}`;
+    }   
+  } 
+
+  return result;
 }
